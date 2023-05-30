5362b4604689c69085749e67ab71e05f6b42895d
fluxfw <fluxfw@fluxlabs.ch>
2023-05-30 14:48:58 +0200

    `flux-ilias-rest-api`

--- bin/install-flux-ilias-rest-api.sh
+++ bin/install-flux-ilias-rest-api.sh
@@ -7,6 +7,6 @@ if [ ! -d "$ILIAS_WEB_DIR/Customizing/global/flux-ilias-rest-api" ]; then
     exit 1
 fi
 
-echo "Link $ILIAS_WEB_DIR/Customizing/global/flux-ilias-rest-api/src/Adapter/Server/Config/nginx.conf to /flux-ilias-nginx-base/src/custom/flux-ilias-rest-api.conf"
+echo "Link $ILIAS_WEB_DIR/Customizing/global/flux-ilias-rest-api/src/nginx.conf to /flux-ilias-nginx-base/src/custom/flux-ilias-rest-api.conf"
 
-ln -sfT "$ILIAS_WEB_DIR/Customizing/global/flux-ilias-rest-api/src/Adapter/Server/Config/nginx.conf" /flux-ilias-nginx-base/src/custom/flux-ilias-rest-api.conf
+ln -sfT "$ILIAS_WEB_DIR/Customizing/global/flux-ilias-rest-api/src/nginx.conf" /flux-ilias-nginx-base/src/custom/flux-ilias-rest-api.conf
